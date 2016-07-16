14d2adfe5b187d94d6cbb3d5056c40a0dd95bad7
zhou-yg <zhou.y.g890@gmail.com>
2016-07-17 00:53:13 +0800

    _

--- src/sprites/man0foot/index.js
+++ src/sprites/man0foot/index.js
@@ -17,7 +17,7 @@ module.exports = function (leftAside) {
   var mySprite = mySpriteFn();
 
   mySprite.x = leftAside ? 55 : 120
-  mySprite.initY = 305
+  mySprite.initY = 304
   mySprite.y = mySprite.initY + (leftAside? 0 : 4)
   mySprite.scale.x = 0.4
   mySprite.scale.y = 0.4
